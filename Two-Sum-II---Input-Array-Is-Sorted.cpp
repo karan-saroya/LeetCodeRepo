109c83db8aa7e6e4b8351ef12676487735be6ebe
karan-saroya <karanveer.saroya@gmail.com>
2026-02-24 11:26:00 +0530

    [167] [Time Beats: 100.00%] [Memory Beats: 32.70%] - LeetPush

--- Two-Sum-II---Input-Array-Is-Sorted.cpp
+++ Two-Sum-II---Input-Array-Is-Sorted.cpp
@@ -1,25 +1,29 @@
 1class Solution {
 2public:
-3    vector<int> twoSum(vector<int>& numbers, int target) {
-4        unordered_map<int, int> has_seen_complement;
-5        size_t  index=1;
-6        vector<int> answer;
-7        for(auto i: numbers)
+3    vector<int> twoSum(vector<int>& numbers, int target)
+4    {
+5        int start=0, end=numbers.size()-1;
+6        vector<int> answer={};
+7        while(start < end)
 8        {
-9            auto iter = has_seen_complement.find(i);
-10            if(iter != has_seen_complement.end())
-11            {
-12                answer.push_back(iter->second);
-13                answer.push_back(index);
-14                break;
-15            }
-16            else
-17            {
-18                // add the complement along with position
-19                has_seen_complement.insert({target-i,index});
-20                index++;
-21            }
-22        }
-23        return answer;
-24    }
-25};
+9            if(numbers[start] + numbers[end] == target)
+10            {
+11                answer.push_back(start+1);
+12                answer.push_back(end+1);
+13                break;
+14            }
+15            else if (numbers[start] + numbers[end] > target)
+16            {
+17                end--;
+18                continue;
+19            }
+20            else
+21            {
+22                start++;
+23                continue;
+24            }
+25        }
+26        return answer;
+27    }
+28    
+29};
