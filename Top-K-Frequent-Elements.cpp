af47addd0fd8d6dda3d4a542602ec72c66ad3702
karan-saroya <karanveer.saroya@gmail.com>
2026-05-15 19:30:12 +0530

    [347] [Time Beats: 34.10%] [Memory Beats: 25.65%] - LeetPush

--- Top-K-Frequent-Elements.cpp
+++ Top-K-Frequent-Elements.cpp
@@ -2,36 +2,29 @@
 2class Solution {
 3public:
 4    vector<int> topKFrequent(vector<int>& nums, int k) {
-5        vector<vector<int> > bucket;
-6        unordered_map<int,int> hashmap;
-7        vector<int> answer;
-8        for(int i=0;i<=nums.size();i++)
-9            bucket.push_back(vector<int>());
-10
-11        for(int i=0; i < nums.size() ; i++)
-12        {
-13            auto iter  = hashmap.find(nums[i]);
-14            if( iter == hashmap.end())
-15                hashmap.insert({nums[i],1});
-16            else
-17                iter->second = iter->second + 1 ;
-18        }
+5        vector<vector<int>> list;
+6        unordered_map<int, int> mapIndex;
+7        for(int i=0;i<nums.size();i++)
+8        {
+9            if(mapIndex.find(nums[i]) != mapIndex.end())
+10            {
+11                list[mapIndex[nums[i]]][1]++;
+12            }
+13            else
+14            {
+15                mapIndex[nums[i]] = list.size();
+16                vector<int> vec{nums[i], 0};
+17                list.push_back(vec);
+18            }
 19
-20        for(auto it: hashmap)
-21        {
-22            (bucket[it.second]).push_back(it.first);
-23        }
-24        
-25        for(auto it = bucket.rbegin(); it != bucket.rend() ; it++ )
-26        {
-27            if (answer.size() == k)
-28                break;
-29            vector<int> list= *it;
-30            for(auto val : list)
-31                answer.push_back(val);
-32        }
-33
-34        return answer;
-35
-36    }
-37};
+20        }
+21        auto cmp = [](vector<int>& a, vector<int>& b){return a[1] > b[1];};
+22        sort(list.begin(), list.end(),cmp);
+23        vector<int> ans;
+24        for(int i=0;i<k;i++)
+25        {
+26            ans.push_back(list[i][0]);
+27        }
+28        return ans;
+29    }
+30};
