c2fbd2e83472e7c0f0677da1a2c7cfc551079d20
karan-saroya <karanveer.saroya@gmail.com>
2026-07-01 23:33:41 +0530

    [1483] [Time Beats: 10.95%] [Memory Beats: 9.96%] - LeetPush

--- Kth-Ancestor-of-a-Tree-Node.java
+++ Kth-Ancestor-of-a-Tree-Node.java
@@ -1,53 +1,56 @@
-1class TreeAncestor {
-2    int[] par;
-3    int LOG= 32;
-4    int[][] dp;
-5    public TreeAncestor(int n, int[] parent) 
-6    {
-7        par = new int[parent.length];
-8        dp = new int[parent.length][LOG];
-9        par = parent;
-10        for(int i=0;i<parent.length;i++)
-11            Arrays.fill(dp[i], -1);
-12        for(int i=0;i<parent.length;i++)
-13            dp[i][0] = parent[i];
-14
-15        for(int k=1;k<LOG;k++)
-16        {
-17            for(int i=0;i<parent.length;i++)
-18            {
-19                int mid = dp[i][k-1];
-20                if(mid != -1)
-21                {
-22                    dp[i][k] = dp[mid][k-1];
-23                }
-24            }
-25
-26        }    
-27
-28    }
-29    
-30    public int getKthAncestor(int node, int k) 
-31    {
-32
-33
-34        for(int i=0;i<LOG && node != -1;i++)
-35        {
-36            if((k & (1 << i)) != 0)
-37            {
-38                node  = dp[node][i];
-39            }
-40        }
-41        return node;
-42
-43
-44        
-45    }
-46    
-47}
-48
-49/**
-50 * Your TreeAncestor object will be instantiated and called as such:
-51 * TreeAncestor obj = new TreeAncestor(n, parent);
-52 * int param_1 = obj.getKthAncestor(node,k);
-53 */
+1class TreeAncestor
+2{
+3	int[][] up;
+4	int LOG =30;
+5	int[] par;
+6	public TreeAncestor(int n, int[] parent) 
+7    	{
+8		up = new int[n][LOG];
+9		par= parent;
+10		for(int i=0;i<n;i++)
+11			Arrays.fill(up[i], -1);
+12		
+13		preprocess();
+14	}
+15	void preprocess()
+16	{
+17		for(int k=0;k<LOG;k++)
+18		{
+19			for(int i=0;i<up.length;i++)
+20			{
+21				if(k == 0)
+22					up[i][0] = par[i];
+23				else
+24				{
+25					if(up[i][k-1] == -1)
+26						up[i][k] = -1;
+27					else
+28						{
+29							up[i][k] = up[up[i][k-1]][k-1];
+30						}
+31				}
+32			}
+33}
+34
+35}
+36
+37
+38
+39
+40
+41int getKthAncestor(int node, int k)
+42{
+43	for(int i=0;i<LOG;i++)
+44	{
+45		if((k>>i & 1) != 0 && node != -1)
+46		{
+47			node = up[node][i];
+48		}
+49		else if(node == -1)
+50			break;
+51	}
+52	return node;
+53}
+54	
+55}
+56
